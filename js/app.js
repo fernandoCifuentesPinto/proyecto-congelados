0d23f310cd0e3b7bd7db6bf94ef41dc5d9149c89
fcifuentesinstacrops <fcifuentes@instacrops.com>
2026-09-24 13:50:52 -0300

    Generar catalogo de productos dinamicamente

--- js/app.js
+++ js/app.js
@@ -3,12 +3,13 @@ const productos = [
         id: 1,
         nombre: "Hamburguesa Wagyu",
         precio: 8000,
-        imagen: "img/hamburguesa.wagyu.jpeg"
+        imagen: "img/hamburguesa-wagyu.jpeg"
     },
     {
         id: 2,
         nombre: "Jamón Pierna",
-        precio: 1000
+        precio: 1000,
+        imagen: "img/jamon-pierna.jpeg"
     }
 ];
 // Seleccionamos la sección del catálogo en el HTML
@@ -16,6 +17,13 @@ const catalogo = document.querySelector(".catalogo");
 console.log(catalogo);
 //Recorremos el array para acceder a los productos
 productos.forEach((producto) => {
-    console.log(producto.nombre, producto.precio)
+    catalogo.innerHTML +=
+    `<article class = "producto">
+        <h2>${producto.nombre}</h2>
+        <img src = "${producto.imagen}" alt = "${producto.nombre}">
+        <p>$${producto.precio.toLocaleString("es-CL")}</p>
+        <button>Agregar al carrito</button>
+    </article>`
 })
+
 
