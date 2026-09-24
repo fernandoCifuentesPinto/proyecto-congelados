af09ad7d288e566b6a98a3532bda6649958ef1de
fcifuentesinstacrops <fcifuentes@instacrops.com>
2026-09-24 01:17:17 -0300

    Agregar array de productos y seleccion del catolog con JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log("hola mi gente");
+const productos = [
+    {
+        id: 1,
+        nombre: "Hamburguesa Wagyu",
+        precio: 8000,
+        imagen: "img/hamburguesa.wagyu.jpeg"
+    },
+    {
+        id: 2,
+        nombre: "Jamón Pierna",
+        precio: 1000
+    }
+];
+// Seleccionamos la sección del catálogo en el HTML
+const catalogo = document.querySelector(".catalogo");
+console.log(catalogo);
+//Recorremos el array para acceder a los productos
+productos.forEach((producto) => {
+    console.log(producto.nombre, producto.precio)
+})
+
